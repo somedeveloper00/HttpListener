2988d1136730a660bb1201b5ee2b55a1b1e87d0c
Saeed Barari <barari.saeed.am@gmail.com>
2024-04-24 10:47:16 +0330

    added support for CORS

--- HttpRouteListenerComponent.cs
+++ HttpRouteListenerComponent.cs
@@ -31,13 +31,42 @@ namespace HttpListener
         }
     }
 
+    [Serializable]
+    public sealed class CorsOptions
+    {
+        public string[] allowedOrigins = new string[] { "*" };
+        public HttpMethodEnum[] allowedMethods = new HttpMethodEnum[] { HttpMethodEnum.POST, HttpMethodEnum.GET, HttpMethodEnum.OPTIONS };
+        public bool allowCredentials = true;
+        public string[] allowedHeaders = new string[]
+        {
+            "Content-Type",
+            "Content-Encoding",
+            "X-Authentication",
+            "X-Authorization",
+            "X-PlayFabSDK",
+            "X-ReportErrorAsSuccess",
+            "X-SecretKey",
+            "X-EntityToken",
+            "Authorization",
+            "x-ms-app",
+            "x-ms-client-request-id",
+            "x-ms-user-id",
+            "traceparent",
+            "tracestate",
+            "Request-Id"
+         };
+        public HttpProcessUnityEvent onProcess;
+    }
+
     public sealed class HttpRouteListenerComponent : MonoBehaviour
     {
         public HttpListenerComponent serverComponent;
         public string path = "/";
         public HttpMethodEnum method;
+        public CorsOptions corsOptions;
         public HttpProcessUnityEvent onProcess;
         private HttpListenerComponent.RouteHandler _routeHandler;
+        private HttpListenerComponent.RouteHandler _corsRouteHandler;
 
         private void OnValidate()
         {
@@ -64,8 +93,31 @@ namespace HttpListener
 
         private void Awake()
         {
-            _routeHandler = new(onProcess.Invoke, method.ToHttpMethod(), path);
+            _routeHandler = new HttpListenerComponent.RouteHandler(onProcess.Invoke, method.ToHttpMethod(), path);
+            _corsRouteHandler = new HttpListenerComponent.RouteHandler(OnCors, HttpMethod.Options, path);
             serverComponent.BindRoute(_routeHandler);
+            serverComponent.BindRoute(_corsRouteHandler);
+        }
+
+        private void OnCors(HttpListenerRequest req, HttpListenerResponse res)
+        {
+            res.AddHeader("Access-Control-Allow-Origin", string.Join(',', corsOptions.allowedOrigins));
+            res.AddHeader("Access-Control-Allow-Methods", string.Join(',', corsOptions.allowedMethods));
+            res.AddHeader("Access-Control-Allow-Credentials", corsOptions.allowCredentials.ToString());
+            res.AddHeader("Access-Control-Allow-Headers", string.Join(',', corsOptions.allowedHeaders));
+            try
+            {
+                corsOptions.onProcess?.Invoke(req, res);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+            
+            if (res.OutputStream.CanWrite)
+            {
+                res.OutputStream.Close();
+            }
         }
 
         private void OnDestroy()
@@ -73,6 +125,7 @@ namespace HttpListener
             if (serverComponent && !serverComponent.IsRunning())
             {
                 serverComponent.UnbindRoute(_routeHandler);
+                serverComponent.UnbindRoute(_corsRouteHandler);
             }
         }
     }
